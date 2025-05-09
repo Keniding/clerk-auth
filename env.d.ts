6b37e45dc695fcdf083c6262e35f57bc3ed7f8d8
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2025-05-08 19:32:50 -0500

    refactor: move global styles to separate file and update middleware
    Moved global CSS styles from Layout.astro to global.css to improve maintainability. Updated middleware to set localized titles based on browser language and added logging for detected languages.

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare namespace App {
+    interface Locals {
+      title: string
+      description: string
+      keywords: string
+      author: string
+      url: string
+      copyright: string
+      version: string 
+    }
+  }
--- src/middleware.ts
+++ src/middleware.ts
@@ -23,16 +23,32 @@ export const onRequest = defineMiddleware(async (context, next) => {
   
   // Detectar idioma del navegador
   const acceptLanguage = context.request.headers.get('accept-language');
+  console.log(`Idioma detectado: ${acceptLanguage}`);
   
   if (acceptLanguage) {
     const fullLanguage = acceptLanguage.split(',')[0];
     const baseLanguage = fullLanguage.split('-')[0];
-    
+
     console.log(`Idioma completo: ${fullLanguage}, Idioma base: ${baseLanguage}`);
     
     // Redirigir a idioma soportado o inglés por defecto
     // const targetLang = supportedLanguages.includes(baseLanguage) ? baseLanguage : 'en';
     // return context.redirect(`/${targetLang}${url.pathname === '/' ? '' : url.pathname}`);
+  }
+  
+  switch (acceptLanguage) {
+    case 'de':
+    case 'fr':
+      break;
+    case 'en':      
+      context.locals.title = 'Hello Astro english';
+      break;
+    case 'es':      
+      context.locals.title = 'Hola Astro español';
+      break;
+    default:        
+      context.locals.title = 'Hello Astro english';
+      break;
   }
   
   return next();
